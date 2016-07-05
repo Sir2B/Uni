30ae363cf2e9114ad0550c59ea314b5bc9e9c61d
Tobias Obermayer <sir2b@gmx.de>
2016-07-05 13:23:13 +0200

    added scope_test Numerik

---
+++ C++/Numerik/scope_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 50;
+    cout << i << endl;
+    for (int i=0; i<5; i++)
+    {
+        cout < i < endl;
+    }
+    cout << i << endl;
+}
+
