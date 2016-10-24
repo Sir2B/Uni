e018656f8cf0d3f3340d3e2f1151601b3eac1b95
Tobias Obermayer <tobias.obermayer@gmail.com>
2016-10-24 13:32:29 +0200

    ~ can now create Video Database

--- Python/Video/video.py
+++ Python/Video/video.py
@@ -3,6 +3,8 @@
 
 import sqlite3
 import os
+import codecs
+
 
 class VideosManager(object):
     """docstring for VideosManager"""
@@ -12,7 +14,7 @@ class VideosManager(object):
         self.table_name = "video"
         self.video_folder = u'D:\Downloads'
         self.movie_extensions = ['.avi', '.mkv']
-        self.default_language = "GER"
+        self.default_language = u"GER"
 
         self.connection = sqlite3.connect(self.db_file, isolation_level='DEFERRED')
         self.cursor = self.connection.cursor()
@@ -24,53 +26,46 @@ class VideosManager(object):
     def aktualisieren(self):
         self.cursor.execute('''DROP TABLE IF EXISTS {0}_tmp'''.format(self.table_name))
         self.cursor.execute('''ALTER TABLE {0} RENAME TO {0}_tmp'''.format(self.table_name))
-        self.createDatabase()
+        self.create_database()
         self.connection.close()
 
-
-    def createDatabase(self):
+    def create_database(self):
         self.cursor.execute('''CREATE TABLE {0} (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         Title TEXT,
         Year INTEGER,
         Filepath TEXT,
-        Release TEXT,
+        ReleaseName TEXT,
         Language TEXT,
         Cover BLOB
         );'''.format(self.table_name))
 
+    def find_new_db_entries(self):
+
         os.chdir(self.video_folder)
-        for root, dirs, files in os.walk('.'):
-            if len(files) is not 0:
-                # print root
-                # print dirs
-                # print files
-                # ordner=root.split('/')[1]
-                # nummer=root.split('/')[2].split('. ')[0]
-                # stueck=root.split('/')[2].split('. ')[1]
-                #name=files[0].split('.jpg')[0]
-                for file in files:
-                    name, extension = os.path.splitext(file)
-                    if (extension in self.movie_extensions):
-                        title, year = self.extractInformationsFromFilename(name)
-                        print title
-                        release = self.getRelease(root)
-                        language = self.getLanguage(root)
-                        blob = self.getCoverBlob(root)
-                        # print title, year, file, release, language
-                        self.cursor.execute('''INSERT INTO {0} VALUES (?, ?, ?, ?, ?, ?, ?)'''.format(self.table_name), 
-                            (None, title, year, file, release, language, None))
-                        self.connection.commit()
-        
-        #try:
-        # self.database.execute('''DROP TABLE {0}_tmp'''.format(self.table_name))
-        # conn.commit()
-        # conn.close()
+        for root_folder in os.listdir(u'.'):
+            if not os.path.isdir(root_folder):
+                continue
+            for afile in os.listdir(root_folder):
+                name, extension = os.path.splitext(afile)
+                if extension in self.movie_extensions:
+                    infos = self.extract_infos_from_filename(name)
+                    if infos is None:
+                        continue
+                    title, year = infos
+                    print title
+                    release = self.get_release(root_folder)
+                    language = self.get_language(root_folder)
+                    blob = self.get_cover_blob(root_folder)
+                    # print title, year, file, release, language
+                    self.cursor.execute('''INSERT INTO {0} VALUES (?, ?, ?, ?, ?, ?, ?)'''.format(self.table_name),
+                                        (None, title, year, root_folder, release, language, blob))
+                    self.connection.commit()
 
     @staticmethod
-    def extractInformationsFromFilename(name):
+    def extract_infos_from_filename(name):
         # name, extension = os.path.splitext(filename)
-        if not (name[-1] is ')' and name[-6] is '('):
+        if not (name[-1] == ')' and name[-6] == '('):
             return None
         year_string = name[-5:-1]
         if not year_string.isdigit():
@@ -79,13 +74,13 @@ class VideosManager(object):
         title = name[0:-7]
         return title, year
 
-    def getRelease(self, folder):
+    def get_release(self, folder):
         filepath = os.path.join(self.video_folder, folder, 'Infos.nfo')
-        with open(filepath, 'r') as f:
+        with codecs.open(filepath, 'r', 'utf-8') as f:
             first_line = f.readline()
             return first_line
 
-    def getLanguage(self, folder):
+    def get_language(self, folder):
         try:
             start_index = folder.index('[')
             end_index = folder.index(']')
@@ -94,20 +89,20 @@ class VideosManager(object):
         language = folder[start_index+1:end_index]
         return language
 
-    def getCoverBlob(self, folder):
+    def get_cover_blob(self, folder):
         path = os.path.join(self.video_folder, folder, 'folder.jpg')
-        with open(path, 'rb') as file:
-            fileblob = file.read()
+        with open(path, 'rb') as afile:
+            fileblob = afile.read()
             return sqlite3.Binary(fileblob)
 
-
     def auslesen(self):
-        self.database.execute('''SELECT * from {0}'''.format(self.table_name))
-        result = curs.fetchone()
+        self.connection.execute('''SELECT * from {0}'''.format(self.table_name))
+        result = self.cursor.fetchone()
         while result is not None:
             print result
-            result = curs.fetchone()
-        conn.close()
+            result = self.cursor.fetchone()
+        self.connection.close()
+
 
 def main():
     manager = VideosManager()
