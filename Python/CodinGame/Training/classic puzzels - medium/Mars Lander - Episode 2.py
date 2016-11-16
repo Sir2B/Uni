859fc304957b6e9f9a28528c59a9bdf43ccd71e5
Tobias Obermayer <tobias.obermayer@gmail.com>
2016-11-16 09:26:06 +0100

    ~ improved mars lander

--- Python/CodinGame/Training/classic puzzels - medium/Mars Lander - Episode 2.py
+++ Python/CodinGame/Training/classic puzzels - medium/Mars Lander - Episode 2.py
@@ -26,38 +26,41 @@ while True:
     print >> sys.stderr, "h_speed: ", h_speed, " v_speed: ", v_speed
     
     angel = 0
-    power = 3
+    thrust = 3
     diff = x-landing_x_optimum
     height = y-landing_y
     print >> sys.stderr, "diff: ", diff, " height: ", height
     
-    if abs(diff) > 500:
+    if landing_x[0] < x < landing_x[1]: # Landevorgang
+        if v_speed < -40 or (v_speed < -10 and height<1000) or abs(h_speed) > 30:
+            thrust = 4
+        
+        if h_speed < -10:
+            angel = -45
+        elif h_speed > 10:
+            angel = 45
+
+        if height < 250:
+            angel = 0
+
+    else:
         if v_speed < 0:
-            power = 4
-        if h_speed < 30 and diff<0:
+            thrust = 4
+
+        if diff<0:
             angel = -30
-        elif h_speed > -30 and diff>0:
+        else:
             angel = 30
+
         if abs(h_speed) >= 50:
             angel = 15*h_speed/abs(h_speed)
         if abs(diff) < 1500 and abs(h_speed) >= 20:
             angel = 15*h_speed/abs(h_speed)
             speed = 3
-    else:
-        if v_speed < -40 or (v_speed < -10 and height<1000) or abs(h_speed) > 30:
-            power = 4
-        else: 
-            power = 2
-        
-        if h_speed < -20:
-            angel = -70
-        elif h_speed > 20:
-            angel = 70
+
+
     if v_speed <= -40:
         angel = 0
-    
-    if height < 250 and (landing_x[0] < x < landing_x[1]):
-        angel = 0
 
-    # rotate power. rotate is the desired rotation angle. power is the desired thrust power.
-    print "{0} {1}".format(angel, power)
+    # rotate thrust. rotate is the desired rotation angle. thrust is the desired thrust power.
+    print "{0} {1}".format(angel, thrust)
