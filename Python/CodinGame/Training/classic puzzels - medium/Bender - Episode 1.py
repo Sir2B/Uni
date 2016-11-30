dabb5cbda5989ea21793471582ed7451fbab5b77
Tobias Obermayer <tobias.obermayer@gmail.com>
2016-11-30 10:03:39 +0100

    ~ breaker_mode not functioning

--- Python/CodinGame/Training/classic puzzels - medium/Bender - Episode 1.py
+++ Python/CodinGame/Training/classic puzzels - medium/Bender - Episode 1.py
@@ -4,21 +4,42 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-def can_go(obstacle):
+
+def can_go(obstacle, position):
+    global breaker_mode
     if obstacle == "#":
         return False
     elif obstacle == "X":
         if breaker_mode:
+            remove_obstacle(position)
             breaker_mode = False
             return True
         return False
     return True
-    
-def redeem_obstacle(obstacle):
+
+
+def remove_obstacle(position):
+    global bender_map
+    line = list(bender_map[position[1]])
+    line[position[0]] = " "
+    bender_map[position[1]] = "".join(line)
+
+
+def teleport_from(position):
+    global teleports, bender_pos
+    for t in teleports:
+        if t != position:
+            bender_pos = t
+
+
+def redeem_obstacle(obstacle, position):
+    global breaker_mode, next_direction, running, bender_dir
     if obstacle == "B":
+        if not breaker_mode:
+            remove_obstacle(position)
         breaker_mode = True
     elif obstacle == "T":
-        pass
+        teleport_from(position)
     elif obstacle == "I":
         next_direction = next_direction[::-1]
     elif obstacle == "$":
@@ -34,11 +55,12 @@ def redeem_obstacle(obstacle):
         
 next_direction = ["SOUTH", "EAST", "NORTH", "WEST"]
 
-direction_map = {"SOUTH": (0,1), "EAST": (1,0), 
-                "NORTH": (0,-1), "WEST":(-1,0)}
+direction_map = {"SOUTH": (0, 1), "EAST": (1, 0),
+                 "NORTH": (0, -1), "WEST": (-1, 0)}
 
 bender_map = []
 bender_pos = ()
+teleports = []
 bender_dir = "SOUTH"
 breaker_mode = False
 l, c = [int(i) for i in raw_input().split()]
@@ -48,25 +70,24 @@ for y in xrange(l):
     if "@" in row:
         x = row.index("@")
         bender_pos = (x, y)
+    elif "T" in row:
+        x = row.index("T")
+        teleports.append((x, y))
     
 print >> sys.stderr, bender_map, bender_pos
 running = True
 next_direction_i = 0
-while (running):
+while running:
     direction = direction_map[bender_dir]
-    next_pos = bender_pos[0]+direction[0],bender_pos[1]+direction[1]
-    next_obs = bender_map[next_pos[0]][next_pos[1]]
-    
-    if can_go(next_obs):
-        print >> sys.stderr, "Obs: {0}".format(next_obs)
-        redeem_obstacle(next_obs)
-        bender_pos = next_pos
+    next_pos = bender_pos[0]+direction[0], bender_pos[1]+direction[1]
+    next_obs = bender_map[next_pos[1]][next_pos[0]]
+
+    if can_go(next_obs, next_pos):
+        next_direction_i = 0
+        # print >> sys.stderr, "Obs: {0}".format(next_obs)
         print bender_dir
+        bender_pos = next_pos
+        redeem_obstacle(next_obs, next_pos)
     else:
         bender_dir = next_direction[next_direction_i]
         next_direction_i += 1
-
-# Write an action using print
-# To debug: print >> sys.stderr, "Debug messages..."
-
-print "answer"
