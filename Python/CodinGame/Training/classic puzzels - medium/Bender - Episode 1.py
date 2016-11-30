f362d6e7584af82c4e65e7de97679591e34191c8
Tobias Obermayer <tobias.obermayer@gmail.com>
2016-11-30 10:55:32 +0100

    ~ everything except loop detection is working

--- Python/CodinGame/Training/classic puzzels - medium/Bender - Episode 1.py
+++ Python/CodinGame/Training/classic puzzels - medium/Bender - Episode 1.py
@@ -1,6 +1,7 @@
 import sys
 import math
 
+
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
@@ -12,14 +13,14 @@ def can_go(obstacle, position):
     elif obstacle == "X":
         if breaker_mode:
             remove_obstacle(position)
-            breaker_mode = False
             return True
         return False
     return True
 
 
 def remove_obstacle(position):
-    global bender_map
+    global bender_map, map_changed
+    map_changed = True
     line = list(bender_map[position[1]])
     line[position[0]] = " "
     bender_map[position[1]] = "".join(line)
@@ -32,12 +33,23 @@ def teleport_from(position):
             bender_pos = t
 
 
+def is_loop(pose):
+    global positions, map_changed
+    posi, diri, breakeri, changed = pose
+    if posi in positions:
+        posi2, diri2, breakeri2, changed2 = positions[posi]
+        if changed or changed2 or diri2 != diri or breakeri != breakeri2:
+            return False
+        return True
+    else:
+        positions[posi] = (diri, breakeri, changed)
+        return False
+
+
 def redeem_obstacle(obstacle, position):
     global breaker_mode, next_direction, running, bender_dir
     if obstacle == "B":
-        if not breaker_mode:
-            remove_obstacle(position)
-        breaker_mode = True
+        breaker_mode = not breaker_mode
     elif obstacle == "T":
         teleport_from(position)
     elif obstacle == "I":
@@ -52,7 +64,8 @@ def redeem_obstacle(obstacle, position):
         bender_dir = "WEST"
     elif obstacle == "N":
         bender_dir = "NORTH"
-        
+
+
 next_direction = ["SOUTH", "EAST", "NORTH", "WEST"]
 
 direction_map = {"SOUTH": (0, 1), "EAST": (1, 0),
@@ -63,6 +76,8 @@ bender_pos = ()
 teleports = []
 bender_dir = "SOUTH"
 breaker_mode = False
+loop = False
+map_changed = False
 l, c = [int(i) for i in raw_input().split()]
 for y in xrange(l):
     row = raw_input()
@@ -73,21 +88,36 @@ for y in xrange(l):
     elif "T" in row:
         x = row.index("T")
         teleports.append((x, y))
-    
+
 print >> sys.stderr, bender_map, bender_pos
 running = True
 next_direction_i = 0
+directions = []
+positions = {}
 while running:
     direction = direction_map[bender_dir]
-    next_pos = bender_pos[0]+direction[0], bender_pos[1]+direction[1]
+    next_pos = bender_pos[0] + direction[0], bender_pos[1] + direction[1]
     next_obs = bender_map[next_pos[1]][next_pos[0]]
 
     if can_go(next_obs, next_pos):
         next_direction_i = 0
         # print >> sys.stderr, "Obs: {0}".format(next_obs)
-        print bender_dir
+        directions.append(bender_dir)
+
+        pos = (bender_pos, bender_dir, breaker_mode, map_changed)
+
+        if is_loop(pos):
+            running = False
+            loop = True
+        positions[bender_pos] = pos
         bender_pos = next_pos
         redeem_obstacle(next_obs, next_pos)
     else:
         bender_dir = next_direction[next_direction_i]
         next_direction_i += 1
+
+if loop:
+    print "LOOP"
+else:
+    for d in directions:
+        print d
