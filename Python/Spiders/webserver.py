0b835ef408a66aa2d20288ab26e55fcf1699bf27
Tobias Obermayer <tobias.obermayer@gmail.com>
2017-04-23 08:05:31 +0200

    ADD|Favicons and Webserver Cache
    Webserver of sauspiel can now send favicons and if you load a statisic multiple times, it will be load from the cache und not generated each time.

--- Python/Spiders/webserver.py
+++ Python/Spiders/webserver.py
@@ -3,16 +3,26 @@
 
 import BaseHTTPServer
 import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
 import pylab
 import io
+import os
+from datetime import datetime
 
+IMAGE_BUFFER = io.BytesIO()
+LAST_MODIFIED_DATE = None
+LAST_CALL = None
 
 class SauspielServer(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         path = self.path
 
-        path_list = path.split('?')
-        if len(path_list)==1:
+        path_list = path.split('/')
+        if path_list[1] == 'favicon.ico':
+            self.send_file('Stats.ico')
+            return
+        if len(path_list) == 1:
             self.send_error()
             return
         self.op = path_list[1]
@@ -21,22 +31,22 @@ class SauspielServer(BaseHTTPServer.BaseHTTPRequestHandler):
         if self.op not in ('all', 'date', 'game'):
             self.send_error()
             return
-        
+
         if self.op == 'game':
-            if len(path_list)!=3:
+            if len(path_list) != 3:
                 self.send_error()
                 return
             self.game_count = int(path_list[2])
 
         # self.read_stats()
-        buffer = self.draw_plot()
+        image_buffer = self.draw_plot()
 
         self.send_response(200)
         self.send_header("Content-type", "image/png")
         self.end_headers()
 
-        self.wfile.write(buffer.read())
-        buffer.close()
+        self.wfile.write(image_buffer.read())
+        # image_buffer.close()
         # for line in self.lines[-5:]:
         #     self.wfile.write(line + '\r\n')
         # self.wfile.write(str(result) + '\r\n')
@@ -54,7 +64,18 @@ class SauspielServer(BaseHTTPServer.BaseHTTPRequestHandler):
             self.lines = s_file.readlines()
 
     def draw_plot(self):
+        global LAST_CALL, LAST_MODIFIED_DATE, IMAGE_BUFFER
         data_filename = "stats.txt"
+        try:
+            mtime = os.path.getmtime(data_filename)
+        except OSError:
+            mtime = 0
+        modified_date = datetime.fromtimestamp(mtime)
+        if LAST_CALL == self.path and modified_date == LAST_MODIFIED_DATE:
+            IMAGE_BUFFER.seek(0)
+            return IMAGE_BUFFER
+        LAST_CALL = self.path
+        LAST_MODIFIED_DATE = modified_date
         data = pylab.genfromtxt(data_filename , delimiter=',', dtype=int)
         y_data = data[:, 0]
         x_data = data[:, 1]
@@ -63,15 +84,22 @@ class SauspielServer(BaseHTTPServer.BaseHTTPRequestHandler):
             x_data = x_data[-self.game_count:]
         pylab.plot(x_data, y_data, '-')
         # pylab.show()
-        buf = io.BytesIO()
-        pylab.savefig(buf, format='png')
-        buf.seek(0)
+        IMAGE_BUFFER = io.BytesIO()
+        pylab.savefig(IMAGE_BUFFER, format='png')
+        IMAGE_BUFFER.seek(0)
         # pylab.legend()
         # pylab.title("Title of Plot")
         # pylab.xlabel("X Axis Label")
         # pylab.ylabel("Y Axis Label")
         pylab.close()
-        return buf
+        return IMAGE_BUFFER
+
+    def send_file(self, filepath):
+        self.send_response(200)
+        self.send_header("Content-type", "image/png")
+        self.end_headers()
+        image_file = open(filepath)
+        self.wfile.write(image_file.read())
 
 def run_server(port=9090):
     server_class = BaseHTTPServer.HTTPServer
@@ -82,4 +110,5 @@ def run_server(port=9090):
     server.serve_forever()
 
 if __name__ == '__main__':
+    os.chdir('/home/pi/sauspiel/')
     run_server(port=50)
