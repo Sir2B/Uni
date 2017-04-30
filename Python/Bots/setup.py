af31379b9af4de3c8e99176968fbf573054930d7
Tobias Obermayer <tobias.obermayer@gmail.com>
2017-04-30 16:57:42 +0200

    CODE|ADD setup routine for my telegram bot

---
+++ Python/Bots/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bots',
+      version='0.1',
+      description='My Bot Collection',
+      author='Tobias Obermayer',
+      author_email='tobias.obermayer@gmail.com',
+      packages=['Telegram'],
+     )
