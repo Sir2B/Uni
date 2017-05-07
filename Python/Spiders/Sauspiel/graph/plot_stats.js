0644fb9b27cf025a5271d58e889819e4c5349afb
Tobias Obermayer <tobias.obermayer@gmail.com>
2017-05-07 20:16:26 +0200

    CODE|CHANGE jquery renderer resizes always centered

--- Python/Spiders/Sauspiel/graph/plot_stats.js
+++ Python/Spiders/Sauspiel/graph/plot_stats.js
@@ -26,19 +26,13 @@ $(document).ready(function () {
         },
         async: false
     })
+
     // Graph Data ##############################################
     var graphData = [{
-            // Visits
             data: stats,
             color: '#71c73e',
-            points: {radius: 1}
+            points: {radius: 500/Number(games)}
         }
-        // , {
-        // 	// Returning Visits
-        // 	data: [ [6, 500], [7, 600], [8, 550], [9, 600], [10, 800], [11, 900], [12, 800], [13, 850], [14, 830], [15, 1000] ],
-        // 	color: '#77b7c5',
-        // 	points: { radius: 4, fillColor: '#77b7c5' }
-        // }
     ];
 
     // Lines Graph #############################################
@@ -46,10 +40,10 @@ $(document).ready(function () {
         series: {
             points: {
                 show: true,
-                radius: 5
             },
             lines: {
-                show: true
+                show: true,
+                radius: 0.01
             },
             shadowSize: 0
         },
@@ -66,54 +60,9 @@ $(document).ready(function () {
             // }
         ,
         yaxis: {
-            // tickSize: 1000,
             min: min_stat
         }
     });
-
-    // Bars Graph ##############################################
-    // $.plot($('#graph-bars'), graphData, {
-    // 	series: {
-    // 		bars: {
-    // 			show: true,
-    // 			barWidth: .9,
-    // 			align: 'center'
-    // 		},
-    // 		shadowSize: 0
-    // 	},
-    // 	grid: {
-    // 		color: '#646464',
-    // 		borderColor: 'transparent',
-    // 		borderWidth: 20,
-    // 		hoverable: true
-    // 	},
-    // 	// xaxis: {
-    // 	// 	tickColor: 'transparent',
-    // 	// 	tickDecimals: 2
-    // 	// },
-    // 	// yaxis: {
-    // 	// 	tickSize: 1000
-    // 	// }
-    // });
-
-    // Graph Toggle ############################################
-    // $('#graph-bars').hide();
-
-    // $('#lines').on('click', function (e) {
-    //     $('#bars').removeClass('active');
-    //     $('#graph-bars').fadeOut();
-    //     $(this).addClass('active');
-    //     $('#graph-lines').fadeIn();
-    //     e.preventDefault();
-    // });
-
-    // $('#bars').on('click', function (e) {
-    //     $('#lines').removeClass('active');
-    //     $('#graph-lines').fadeOut();
-    //     $(this).addClass('active');
-    //     $('#graph-bars').fadeIn().removeClass('hidden');
-    //     e.preventDefault();
-    // });
 
     // Tooltip #################################################
     function showTooltip(x, y, contents) {
