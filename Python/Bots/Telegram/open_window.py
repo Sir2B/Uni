e9c9b064180fb5ab21fadff49e258196ba54671a
Tobias Obermayer <tobias.obermayer@gmail.com>
2017-07-09 21:37:13 +0200

    activate pull down resistor, check if it's raining for the next hour only, message with precipitation

--- Python/Bots/Telegram/open_window.py
+++ Python/Bots/Telegram/open_window.py
@@ -3,15 +3,16 @@ import pyowm
 import RPi.GPIO as GPIO
 import schedule
 import time
+import datetime
 
 from credentials import Credentials
 
 
 class Raborutzi(object):
     def __init__(self, credentials_path='.credentials', window_gpis=[]):
-        self.weather = None
+        self.forecaster = None
         self.UPDATE_FORECAST_TIME = 60 * 60  # every hour
-        self.CHECK_TIME = 5 *60 # every 5 minutes
+        self.CHECK_TIME = 2 * 60 # every 2 minutes
         self.credentials = Credentials(credentials_path)
 
         self.bot = telebot.TeleBot(self.credentials.TOKEN, threaded=False)
@@ -20,7 +21,7 @@ class Raborutzi(object):
 
         self.window_gpis = window_gpis
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(window_gpis, GPIO.IN)
+        GPIO.setup(window_gpis, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def start_scheduler(self):
         schedule.every(self.UPDATE_FORECAST_TIME).seconds.do(self.update_forecast)
@@ -34,13 +35,14 @@ class Raborutzi(object):
 
     def update_forecast(self):
         self.send("updating forecast")
-        self.weather = self.owm.three_hours_forecast("Tulling,de")
+        self.forecaster = self.owm.three_hours_forecast("Tulling,de")
 
     def is_it_raining(self):
-        if not self.weather:
+        if not self.forecaster:
             self.update_forecast()
-        raining = self.weather.will_have_rain()
-        return raining
+        self.weather = self.forecaster.get_weather_at(datetime.datetime.now())
+        self.rain = self.weather.get_rain()['3h']
+        return self.rain > 0
 
     def is_one_window_open(self):
         for gpi in self.window_gpis:
@@ -50,7 +52,9 @@ class Raborutzi(object):
 
     def check(self):
         if self.is_one_window_open() and self.is_it_raining():
-            self.send("Warning: It's raining and the window is open")
+            warning = "Warning: It's raining and the window is open.\n"
+            warning += "Weather: {}; Rain: {} mm".format(self.weather.get_detailed_status(), self.rain)
+            self.send(warning)
 
 
 if __name__ == "__main__":
